c7b36042ad19923e39ccb4f2e69435f191502aba
Deonte LaBorde <deonte.laborde@gmail.com>
2022-09-07 21:13:40 -0400

    created appointment model and appointments table

---
+++ migrations/20220908011201-create-appointment.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('appointments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATEONLY
+      },
+      time: {
+        type: Sequelize.TIME
+      },
+      reasonForVisit: {
+        type: Sequelize.TEXT
+      },
+      customerId: {
+        type: Sequelize.INTEGER
+      },
+      carId: {
+        type: Sequelize.INTEGER
+      },
+      businessId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('appointments');
+  }
+};
