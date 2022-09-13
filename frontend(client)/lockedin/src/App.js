12d9054948f0b64c9e70496f201cd9f94448651c
Deonte LaBorde <deonte.laborde@gmail.com>
2022-09-13 11:27:20 -0400

    create homepage

--- frontend(client)/lockedin/src/App.js
+++ frontend(client)/lockedin/src/App.js
@@ -1,10 +1,23 @@
+import './App.css'
+import './style.css'
+import {Routes,Route} from 'react-router-dom'
+import {useEffect,useState} from 'react'
+import axios from 'axios'
+import {BASE_URL} from './globals'
 
-import './App.css';
+import Home from './pages/Home'
+
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+      
+  
+     <main>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+     </main>
     </div>
   );
 }
---
+++ frontend(client)/lockedin/src/pages/Home.js
@@ -0,0 +1,22 @@
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to LockedIn</h1>
+      <h3>Need more automotive customers?</h3>
+      <h3> Need more automotive experts?</h3>
+      <h3>Lock in an appointment with LockedIn</h3>
+      <div className="home-grid">
+        <div class="row">
+          <div class="column">
+            <h3>For Customers</h3>
+          </div>
+          <div class="column">
+            <h3> For Businesses</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
