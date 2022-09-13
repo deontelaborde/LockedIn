240405113e358dd9890689137a701578d9e6c246
Deonte LaBorde <deonte.laborde@gmail.com>
2022-09-13 16:29:57 -0400

    clearing git

--- routes/CarRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router()
-const controller = require('../controllers/CarController')
-
-Router.get('/', controller.GetCars)
-Router.get('/:car_id', controller.GetCarById)
-Router.put('/:car_id', controller.UpdateCar)
-Router.delete('/:car_id', controller.DeleteCar)
-
-module.exports = Router
