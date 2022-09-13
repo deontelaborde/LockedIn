9224b93d00b9bf39472dec8798b4303f2f1f1c76
Deonte LaBorde <deonte.laborde@gmail.com>
2022-09-13 16:41:56 -0400

    add login and signup buttons to nav bar

--- frontend(client)/lockedin/src/components/Nav.jsx
+++ frontend(client)/lockedin/src/components/Nav.jsx
@@ -5,9 +5,13 @@ return (
   <header>
     <nav>
       <h1>LockedIn</h1>
+      <Link to="/" className='link'><button className='nav-button'> Home </button></Link>
+      <Link to="/login" className='link'><button className='nav-button'> Login </button></Link>
+      <Link to="/register" className='link'><button className='nav-button'> Sign Up </button></Link>
     </nav>
   </header>
 )
+
 
 }
 
