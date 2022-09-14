43a660ed65e37f13a7ff8318ba0a3e8da6ce5986
Deonte LaBorde <deonte.laborde@gmail.com>
2022-09-14 11:18:45 -0400

    add car form

--- frontend(client)/lockedin/src/components/CarForm.jsx
+++ frontend(client)/lockedin/src/components/CarForm.jsx
@@ -1,18 +1,22 @@
 
 import { useNavigate } from 'react-router-dom';
 
-const CarForm = ({ user, authenticated, reviewFromState, handleReviewChange, handleReviewSubmit }) => {
+const CarForm = ({ user, authenticated, carFromState, handleCarChange, handleCarSubmit }) => {
 
   let navigate = useNavigate();
 
   return (user && authenticated) ? (
     <div className="car-form-container">
       <h3 className='car-form-title'>Add a Car</h3>
-      <form className='car-form form' onSubmit={handleReviewSubmit}>
+      <form className='car-form form' onSubmit={handleCarSubmit}>
+      <input id= 'year' value ={carFromState.year} onChange={handleCarChange} name="year" placeholder='Year'/>
+           <input id= 'make' value ={carFromState.make} onChange={handleCarChange} name="make" placeholder='Make'/>
+           <input id= 'model' value ={carFromState.model} onChange={handleCarChange} name="model" placeholder='Model'/>
+           <input id= 'color' value ={carFromState.color} onChange={handleCarChange} name="color" placeholder='Color'/>
         
         <div className="car-form-row">
       
-          <button className="submit-button button">Submit</button>
+          <button className="submit-button button">Add</button>
         </div>
       </form>
     </div>
