d088a29819167276fc464a4f56f36283480c90a8
Deonte LaBorde <deonte.laborde@gmail.com>
2022-09-20 03:21:09 -0400

    make it better

--- frontend(client)/lockedin/src/App.js
+++ frontend(client)/lockedin/src/App.js
@@ -4,24 +4,25 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { BASE_URL } from './globals'
-
-import Welcome from './pages/Welcome'
 import { Nav } from './components/Nav'
-import BusinessRegister from './pages/BusinessRegister'
 import CarForm from './components/CarForm'
+import Footer from './components/Footer'
+
+import BusinessRegister from './pages/BusinessRegister'
+import Welcome from './pages/Welcome'
 import CustomerHome from './pages/CustomerHome'
 import CustomerDetails from './pages/CustomerDetails'
 import BusinessLogin from './pages/BusinessLogin'
 import EditCar from './pages/EditCar'
+import Profile from './pages/Profile'
+
 import {
   CheckSession,
   CreateCar,
   DestroyCar,
   UpdateCar
 } from './services/Authorize'
-import Profile from './pages/Profile'
 
-import Footer from './components/Footer'
 
 function App() {
   const [authenticated, toggleAuthenticated] = useState(false)
@@ -105,91 +106,91 @@ function App() {
       window.location.reload()
     }
   }
-    const editCar = (car, index) => {
-      setEditing(true)
-      setCar(car)
-      setCarFormState(car)
-      navigate('/cars/edit', { state: { index: index } })
-    }
-
-    const deleteCar = async (car_id) => {
-      await DestroyCar(car_id)
-      navigate('/')
-      window.location.reload()
-    }
-
-    return (
-      <div className="App">
-        <header>
-          <Nav
-            authenticated={authenticated}
-            user={user}
-            handleLogOut={handleLogOut}
-          />
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route
-              path="/profile"
-              element={<Profile user={user} authenticated={authenticated} />}
-            />
-
-            <Route path="/register" element={<BusinessRegister />} />
-            <Route
-              path="/login"
-              element={
-                <BusinessLogin
-                  setUser={setUser}
-                  toggleAuthenticated={toggleAuthenticated}
-                />
-              }
-            />
-            <Route
-              path="/customers"
-              element={
-                <CustomerHome
-                  customers={customers}
-                  selectCustomer={selectCustomer}
-                  setUser={setUser}
-                  toggleAuthenticated={toggleAuthenticated}
-                />
-              }
-            />
-            <Route
-              path="/customers/:customer_id"
-              element={
-                <CustomerDetails
-                  selectedCustomer={selectedCustomer}
-                  authenticated={authenticated}
-                  user={user}
-                  handleCarChange={handleCarChange}
-                  handleCarSubmit={handleCarSubmit}
-                  carFromState={carFromState}
-                  deleteCar={deleteCar}
-                  editCar={editCar}
-                />
-              }
-            />
-           <Route path="/car" element={<CarForm />} />
-            <Route
-              path="/cars/edit"
-              element={
-                <EditCar
-                  car={car}
-                  reviewFromState={carFromState}
-                  onChange={handleCarChange}
-                  onSubmit={handleCarSubmit}
-                />
-              }
-            />
-          </Routes>
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    )
+  const editCar = (car, index) => {
+    setEditing(true)
+    setCar(car)
+    setCarFormState(car)
+    navigate('/cars/edit', { state: { index: index } })
   }
+
+  const deleteCar = async (car_id) => {
+    await DestroyCar(car_id)
+    navigate('/')
+    window.location.reload()
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <Nav
+          authenticated={authenticated}
+          user={user}
+          handleLogOut={handleLogOut}
+        />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route
+            path="/profile"
+            element={<Profile user={user} authenticated={authenticated} />}
+          />
+
+          <Route path="/register" element={<BusinessRegister />} />
+          <Route
+            path="/login"
+            element={
+              <BusinessLogin
+                setUser={setUser}
+                toggleAuthenticated={toggleAuthenticated}
+              />
+            }
+          />
+          <Route
+            path="/customers"
+            element={
+              <CustomerHome
+                customers={customers}
+                selectCustomer={selectCustomer}
+                setUser={setUser}
+                toggleAuthenticated={toggleAuthenticated}
+              />
+            }
+          />
+          <Route
+            path="/customers/:customer_id"
+            element={
+              <CustomerDetails
+                selectedCustomer={selectedCustomer}
+                authenticated={authenticated}
+                user={user}
+                handleCarChange={handleCarChange}
+                handleCarSubmit={handleCarSubmit}
+                carFromState={carFromState}
+                deleteCar={deleteCar}
+                editCar={editCar}
+              />
+            }
+          />
+          <Route path="/car" element={<CarForm />} />
+          <Route
+            path="/cars/edit"
+            element={
+              <EditCar
+                car={car}
+                reviewFromState={carFromState}
+                onChange={handleCarChange}
+                onSubmit={handleCarSubmit}
+              />
+            }
+          />
+        </Routes>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
 
 export default App
